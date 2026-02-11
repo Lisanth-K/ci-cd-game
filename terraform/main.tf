64337211d4974f67ae23a4b96999f044bb4216ee
NISHOK S <nishoks.23csd@kongu.edu>
2026-02-11 10:28:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "game_server" {
   ami           = "ami-019715e0d74f695be" 
-  instance_type = "t3.samll"
+  instance_type = "t3.small"
   key_name      = "jenkinkey"
 
   security_groups = [aws_security_group.mygame.name]
