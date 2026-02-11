95267977d38400f3580366389567e9be399afaf4
LISANTH K <lisanthk.23csd@kongu.edu>
2026-02-11 11:31:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "game_server" {
   ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.small"
-  key_name      = "jenkinkey"
+  key_name      = "lis"
 
   security_groups = [aws_security_group.mygame.name]
 
